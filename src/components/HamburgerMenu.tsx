cc6c08de4e64dfdb5ac810471be197c988d7849f
elidvenega <venegaelid@gmail.com>
2023-09-19 21:02:24 -0700

    Updated

---
+++ src/components/HamburgerMenu.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+
+export default function HamburgerMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`hamburger-menu ${isOpen ? "open" : ""}`}>
+      <div className="hamburger-icon" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <ul className="menu-items">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/services">Services</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </div>
+  );
+}
+
+
