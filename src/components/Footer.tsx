71398bfb7ec0ef290c9c1144683c27f2f27700e7
elidvenega <robotsmailNinja@gmail.com>
2023-11-26 17:00:17 -0800

    Updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,37 +3,57 @@ import Instagram from "../assets/images/icon-instagram.svg";
 import Twitter from "../assets/images/icon-twitter.svg";
 import Pinterest from "../assets/images/icon-pinterest.svg";
 
-export default function Footer() {
+interface footerNavLinks {
+  linkOne: string;
+  linkTwo: string;
+  linkThree: string;
+  linkFour: string;
+  linkFive: string;
+  linkSix: string;
+  linkSeven: string;
+}
+
+const footerData: footerNavLinks = {
+  linkOne: `About`,
+  linkTwo: `Services`,
+  linkThree: `Projects`,
+  linkFour: `https://www.facebook.com/`,
+  linkFive: `https://www.instagram.com/`,
+  linkSix: `https://twitter.com/`,
+  linkSeven: `https://www.pinterest.com/`,
+};
+
+export default function Footer({ footerTitle }: { footerTitle: string }) {
   return (
     <>
       <footer className="footer" id="bottom">
-        <h4>sunnyside</h4>
+        <h4>{footerTitle}</h4>
         <div className="footer-nav">
           <nav>
             <ul>
               <li>
-                <a href="#">About</a>
+                <a href="#">{footerData.linkOne}</a>
               </li>
               <li>
-                <a href="#">Services</a>
+                <a href="#">{footerData.linkTwo}</a>
               </li>
               <li>
-                <a href="#">Projects</a>
+                <a href="#">{footerData.linkThree}</a>
               </li>
             </ul>
           </nav>
         </div>
         <div className="footer-image-links">
-          <a href="https://www.facebook.com/" target="_blank">
+          <a href={footerData.linkFour} target="_blank">
             <img src={Facebook} alt="facebook" />
           </a>
-          <a href="https://www.instagram.com/" target="_blank">
+          <a href={footerData.linkFive} target="_blank">
             <img src={Instagram} alt="instagram" />
           </a>
-          <a href="https://twitter.com/" target="_blank">
+          <a href={footerData.linkSix} target="_blank">
             <img src={Twitter} alt="twitter" />
           </a>
-          <a href="https://www.pinterest.com/" target="_blank">
+          <a href={footerData.linkSeven} target="_blank">
             <img src={Pinterest} alt="pinterest" />
           </a>
         </div>
